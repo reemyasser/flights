692984563d6744cd8749f50db04aa0a44d821780
reemyasser <47141056+reemyasser@users.noreply.github.com>
2022-04-01 20:02:27 +0200

    first commit

---
+++ flights/flights/Areas/Flights/Controllers/flightController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace flights.Areas.Flights.Controllers
+{
+    [Area("Flights")]
+
+    [Authorize]
+
+    public class flightController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult show()
+        {
+            return View();
+        }
+  
+        public IActionResult Booking()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+     
+    }
+}
