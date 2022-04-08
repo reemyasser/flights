30b4facc0e5694de18e4cf4fb68ebabb101e916b
reemyasser <47141056+reemyasser@users.noreply.github.com>
2022-04-08 13:47:35 +0200

    Delete flights directory

--- flights/flights/Areas/Admin/Models/ProjectRole.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace flights.Areas.Admin.Models
-{
-    public class ProjectRole
-    {
-        public int Id { get; set;  }
-         public string RoleName { get; set; }
-    }
-}
--- flights/flights/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace flights.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
